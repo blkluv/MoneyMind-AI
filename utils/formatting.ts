376cd1ae0bf99c50fbe15632de2eababdb47a29f
WEB5D <info@blkluv.org>
2025-12-11 02:20:34 -0500

    Update formatting.ts

--- utils/formatting.ts
+++ utils/formatting.ts
@@ -1,8 +1,7 @@
-
 export const formatCurrency = (amount: number): string => {
-  return new Intl.NumberFormat('en-IN', {
+  return new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'INR',
+    currency: 'USD',
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   }).format(amount);
